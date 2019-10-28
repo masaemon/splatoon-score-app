b6a37a67731d7b0b94a95e6f2bba7f46734b2b4c
masaemon <masaemonbombardier@gmail.com>
2019-10-29 02:12:36 +0900

    add new score page

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,4 +1,7 @@
 class ScoreController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'score/index'
+  get 'score/index' => 'score#index'
+  get 'score/new' => 'score#new'
   get '/' => 'home#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
