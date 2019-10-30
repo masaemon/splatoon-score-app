b55e487ce927f8207bccea34d1321edc3269cf15
masaemon <masaemonbombardier@gmail.com>
2019-10-31 01:04:43 +0900

    add display database score

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,7 +1,22 @@
 class ScoreController < ApplicationController
   def index
+    @score = Score.all.order(created_at: :desc)
   end
 
   def new
+  end
+
+  def create
+    score = Score.new(
+      rule: params["rule"],
+      stage: params["stage"],
+      weapon: params["weapon"],
+      power: params["power"],
+      buttle: params["buttle"]
+    )
+
+    if score.save
+      redirect_to("/score/index")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'score/index' => 'score#index'
   get 'score/new' => 'score#new'
-  get '/' => 'home#index' 
+  post 'score/create' => 'score#create'
+
+  get '/' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
