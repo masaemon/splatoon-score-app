177c9639a500fc407f8eefb4a9fd8961d5e3d091
masaemon <masaemonbombardier@gmail.com>
2019-10-31 01:46:26 +0900

    add update score mode

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -24,6 +24,19 @@ class ScoreController < ApplicationController
     @score = Score.find_by(id: params[:id])
   end
 
+  def update
+    score = Score.find_by(id: params[:id])
+    score.rule = params["rule"]
+    score.stage = params["stage"]
+    score.weapon = params["weapon"]
+    score.power = params["power"]
+    score.buttle = params["buttle"]
+
+    if score.save
+      redirect_to("/score/index")
+    end
+  end
+
   def delete
     score = Score.find_by(id: params[:id])
     score.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'score/index' => 'score#index'
   get 'score/new' => 'score#new'
   post 'score/create' => 'score#create'
+  post 'score/:id/update' => 'score#update'
+  get 'score/:id/edit' => 'score#edit'
 
   get '/' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
